058f8c59c844a7d802bda3e9965b2f5eb8f3fbc7
助名直人 <naoto.s@mba.local>
2020-12-24 21:33:27 +0900

    [Finish] JS-7

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-  $('.box1').css({
-    'background-color': '#0000ff',
-    'height': '100px'
+  $('.box1').on('click', function () {
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function () {
+    $('.box1').removeClass('box1-ext');
   });
 });
